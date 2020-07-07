8403c7cc115dd55e4c449f1b24bd26c56e50cff5
SleepingJane <shikova.eugenia@bk.ru>
2020-07-08 01:01:58 +0300

    use localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from "./components/Navbar";
 import { TodoForm } from "./components/TodoForm";
 import {TodoList} from "./components/TodoList";
 import {ITodo} from "./interfaces";
 
+declare var confirm: (question: string) => boolean
+
 const App: React.FunctionComponent = () => {
    const [todos, setTodos] = useState<ITodo[]>([])
+
+   useEffect(() => {
+      const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+      setTodos(saved)
+   }, [])
+
+   useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+   }, [todos])
 
    const addHandler = (title: string) => {
       const newTodo: ITodo = {
@@ -27,7 +38,7 @@ const App: React.FunctionComponent = () => {
    }
 
    const removeHandler = (id: number) => {
-      const shoudRemove = window.confirm('Вы уверены, что хотите удалить элемент?')
+      const shoudRemove = confirm('Вы уверены, что хотите удалить элемент?')
       if (shoudRemove) {
          setTodos(prev => prev.filter(todo => todo.id !== id))
       }
