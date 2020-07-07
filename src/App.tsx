1dfb3224ea35e7cf3ccbd6717cb7c64b77f08fbc
SleepingJane <shikova.eugenia@bk.ru>
2020-07-08 01:19:15 +0300

    use route

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Navbar } from "./components/Navbar";
-import { TodoForm } from "./components/TodoForm";
-import {TodoList} from "./components/TodoList";
-import {ITodo} from "./interfaces";
-
-declare var confirm: (question: string) => boolean
+import {TodosPage} from "./pages/TodosPage";
+import {AboutPage} from "./pages/AboutPage";
 
 const App: React.FunctionComponent = () => {
-   const [todos, setTodos] = useState<ITodo[]>([])
-
-   useEffect(() => {
-      const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
-      setTodos(saved)
-   }, [])
-
-   useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify(todos))
-   }, [todos])
-
-   const addHandler = (title: string) => {
-      const newTodo: ITodo = {
-         title: title,
-         id: Date.now(),
-         completed: false
-      }
-      //setTodos([newTodo, ...todos])
-      setTodos(prev => [newTodo, ...prev])
-   }
-
-   const toggleHandler = (id: number) => {
-      setTodos(prev => prev.map(todo => {
-         if (todo.id === id) {
-            todo.completed = !todo.completed
-         }
-         return todo
-      }))
-   }
-
-   const removeHandler = (id: number) => {
-      const shoudRemove = confirm('Вы уверены, что хотите удалить элемент?')
-      if (shoudRemove) {
-         setTodos(prev => prev.filter(todo => todo.id !== id))
-      }
-   }
-
    return (
-      <>
+      <BrowserRouter>
          <Navbar />
          <div className="container">
-            <h1>Мой список дел</h1>
-            <TodoForm onAdd={addHandler}/>
-            <TodoList todos={todos}
-                      onToggle={toggleHandler}
-                      onRemove={removeHandler}/>
+            <Switch>
+               <Route component={TodosPage} path="/" exact></Route>
+               <Route component={AboutPage} path="/about"></Route>
+            </Switch>
          </div>
-      </>
+      </BrowserRouter>
    );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 
 export const Navbar: React.FunctionComponent = () => {
     return (<nav>
         <div className="nav-wrapper deep-purple lighten-3 px1">
             <a href="/" className="brand-logo">Just do it</a>
             <ul className="right hide-on-med-and-down">
-                <li><a href="/">Список дел</a></li>
-                <li><a href="/">Информация</a></li>
+                <li>
+                    <NavLink to="/">Список дел</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">Информация</NavLink>
+                </li>
             </ul>
         </div>
     </nav>);
